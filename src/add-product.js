ef2043913339c917322f91b697cfb2624f263b40
johndulf <johndulfmendiola14@gmail.com>
2023-06-29 13:36:03 +0800

    mana

--- src/add-product.js
+++ src/add-product.js
@@ -12,7 +12,19 @@ createApp({
       data.append("method", "addProduct");
       axios.post("../api/index.php", data).then((res) => {
         if (res.data == 1) {
-          alert("Added successfully!");
+          const successMessage = document.createElement("div");
+      successMessage.innerText = "Added successfully!";
+      successMessage.style.textAlign = "center";
+      successMessage.style.position = "fixed";
+      successMessage.style.top = "50%";
+      successMessage.style.left = "50%";
+      successMessage.style.transform = "translate(-50%, -50%)";
+      successMessage.style.backgroundColor = "#eaf7ea";
+      successMessage.style.padding = "10px";
+      successMessage.style.border = "1px solid #2ecc71";
+      successMessage.style.borderRadius = "5px";
+      successMessage.style.zIndex = "9999";
+      document.body.appendChild(successMessage);
           e.target.reset();
           window.location.href = "../back-end/products.php";
         } else {
